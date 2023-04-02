c18e91e9b481fb8a813f9d8546e40cfa8d65cd60
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-02 15:29:34 -0700

    added high score table and sorting

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,9 @@ var answer1;
 var answer2;
 var answer3;
 var answer4;
+var finalScore;
+var quizScore;
+var hsTable;
 
 window.onload = function() {
     hsButton = document.getElementById("hs-button");
@@ -33,6 +36,8 @@ window.onload = function() {
     playAgainButton.addEventListener("click", quiz.startQuiz);
     startButton = document.getElementById("start-button");
     startButton.addEventListener("click", quiz.startQuiz);
+    quizScore = document.getElementById("quiz-score");
+    hsTable = document.getElementById("hs-table");
     question = document.getElementById("question");
     answer1 = document.getElementById("answer-button-1");
     answer1.addEventListener("click", function() {
@@ -51,9 +56,9 @@ window.onload = function() {
         quiz.checkAnswer(parseInt(answer4.dataset.question))
     });
     question1 = ["question1", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    question2 = ["question2", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    question3 = ["question3", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    question4 = ["question4", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    question2 = ["question2", "wrong1", "correctAnswer", "wrong2", "wrong3", "correctAnswer"];
+    question3 = ["question3", "correctAnswer", "wrong1", "wrong2", "wrong3", "correctAnswer"];
+    question4 = ["question4", "wrong1", "wrong2", "correctAnswer", "wrong3", "correctAnswer"];
     question5 = ["question5", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
     quizContent = [question1, question2, question3, question4, question5];
     form = document.querySelector('form');
@@ -61,7 +66,7 @@ window.onload = function() {
         event.preventDefault();
         initialsScore = {
             initials: document.getElementById("initials").value,
-            score: timeLeft
+            score: finalScore
         }
         localStorage.setItem(Date.now().toString(), JSON.stringify(initialsScore));
         console.log(JSON.stringify(initialsScore));
@@ -73,6 +78,8 @@ var quiz = {
     startQuiz: function() {
         console.log("startGame ran");
         timeLeft = 60;
+        finalScore = 0;
+        countdown();
         startScreen.style.display = "none";
         endScreen.style.display = "none";
         hsScreen.style.display = "none";
@@ -97,7 +104,12 @@ var quiz = {
     },
     wrongAnswer: function() {
         currentQuestion++;
-        quiz.nextQuestion();
+        timeLeft -= 15;
+        if (timeLeft > 0) {
+            quiz.nextQuestion();
+        } else {
+            quiz.endQuiz();
+        }
     },
     nextQuestion: function() {
         if (quizContent[currentQuestion] === undefined) {
@@ -115,16 +127,46 @@ var quiz = {
         quizArea.style.display = "none";
         hsScreen.style.display = "none";
         endScreen.style.display = "block";
+        finalScore = timeLeft;
+        if (timeLeft > 0) {
+            timeLeft = -1;
+        } else {
+            finalScore = 0;
+        }
+        quizScore.textContent = finalScore;
     },
     showHS: function() {
         startScreen.style.display = "none";
         quizArea.style.display = "none";
         endScreen.style.display = "none";
         hsScreen.style.display = "block";
+
+        var highScoresArr = [];
+        for (i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let value = JSON.parse(localStorage.getItem(key));
+            highScoresArr.push([value.initials, parseInt(value.score)]);
+        }
+        highScoresArr.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+        for (i = hsTable.rows.length - 1; i > 0; i--) {
+            hsTable.deleteRow(i);
+        }
+        for (let i = 0; i < highScoresArr.length; i++) {
+            var row = document.createElement('tr');
+            var initialsCell = document.createElement('td');
+            var scoreCell = document.createElement('td');
+          
+            initialsCell.textContent = highScoresArr[i][0];
+            scoreCell.textContent = highScoresArr[i][1];
+          
+            row.appendChild(initialsCell);
+            row.appendChild(scoreCell);
+            hsTable.appendChild(row);
+        }
     }
-
 }
-
 
 function countdown() {
     var timeInterval = setInterval(function () {
@@ -135,7 +177,7 @@ function countdown() {
         timer.textContent = ":^)";
         clearInterval(timeInterval);
       } else {
-        game.loseGame();
+        quiz.endQuiz();
         timer.textContent = ":^(";
         clearInterval(timeInterval);
       }
