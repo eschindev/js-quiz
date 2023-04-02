71970942287b42b741d253104f60cbe9d94fe1c1
eschindev <118208710+eschindev@users.noreply.github.com>
2023-04-02 13:16:30 -0700

    fixed screen change to high scores, saving scores to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-
 var timeLeft;
+var initialsScore;
 var timer;
 var quizArea;
 var startScreen;
 var endScreen;
+var hsButton;
 var hsScreen;
+var playAgainButton;
 var startButton;
 var quizContent;
 var currentQuestion;
@@ -20,41 +22,50 @@ var answer3;
 var answer4;
 
 window.onload = function() {
-    var timeLeft;
-    var timer = document.getElementById("timer");
-    var quizArea = document.getElementById("quiz-area");
-    var startScreen = document.getElementById("start-screen");
-    var endScreen = document.getElementById("end-screen");
-    var hsScreen = document.getElementById("hs-screen");
-    var startButton = document.getElementById("start-button");
+    hsButton = document.getElementById("hs-button");
+    hsButton.addEventListener("click", quiz.showHS);
+    timer = document.getElementById("timer");
+    quizArea = document.getElementById("quiz-area");
+    startScreen = document.getElementById("start-screen");
+    endScreen = document.getElementById("end-screen");
+    hsScreen = document.getElementById("hs-screen");
+    playAgainButton = document.getElementById("play-again-button");
+    playAgainButton.addEventListener("click", quiz.startQuiz);
+    startButton = document.getElementById("start-button");
     startButton.addEventListener("click", quiz.startQuiz);
-    var question = document.getElementById("question");
-    var answer1 = document.getElementById("answer-button-1");
+    question = document.getElementById("question");
+    answer1 = document.getElementById("answer-button-1");
     answer1.addEventListener("click", function() {
         quiz.checkAnswer(parseInt(answer1.dataset.question))
     });
-    var answer2 = document.getElementById("answer-button-2");
+    answer2 = document.getElementById("answer-button-2");
     answer2.addEventListener("click", function() {
         quiz.checkAnswer(parseInt(answer2.dataset.question))
     });
-    var answer3 = document.getElementById("answer-button-3");
+    answer3 = document.getElementById("answer-button-3");
     answer3.addEventListener("click", function() {
         quiz.checkAnswer(parseInt(answer3.dataset.question))
     });
-    var answer4 = document.getElementById("answer-button-4");
+    answer4 = document.getElementById("answer-button-4");
     answer4.addEventListener("click", function() {
         quiz.checkAnswer(parseInt(answer4.dataset.question))
     });
-    var question1 = ["question1", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    var question2 = ["question2", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    var question3 = ["question3", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    var question4 = ["question4", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    var question5 = ["question5", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
-    var quizContent = [question1, question2, question3, question4, question5];
-    var form = document.querySelector('form');
+    question1 = ["question1", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    question2 = ["question2", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    question3 = ["question3", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    question4 = ["question4", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    question5 = ["question5", "wrong1", "wrong2", "wrong3", "correctAnswer", "correctAnswer"];
+    quizContent = [question1, question2, question3, question4, question5];
+    form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        // Your code here
+        initialsScore = {
+            initials: document.getElementById("initials").value,
+            score: timeLeft
+        }
+        localStorage.setItem(Date.now().toString(), JSON.stringify(initialsScore));
+        console.log(JSON.stringify(initialsScore));
+        quiz.showHS();
     });
 }
 
